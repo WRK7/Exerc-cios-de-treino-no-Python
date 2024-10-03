ef627d146d42afb77f3641bc72bb15c9edca1c77
WRK7 <wesleyc09gomes@gmail.com>
2024-10-03 12:57:20 -0400

    Primeiros exercícios de treino para Lista

---
+++ LISTAS2.py
@@ -0,0 +1,43 @@
+'''a = [1, 2, 3, 4, 5]
+
+lista_qadrada = [x**2 for x in a] #Para cada elemento "x" na lista, eleva ao quadrado e armazena em uma nova lista que foi nomeada de "lista_quadrada"
+
+print (lista_qadrada)'''
+
+'''b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 
+39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+
+
+
+
+lista_par = [x for x in b if x %2 == 0] 
+
+# "x %2 == 0" verifica se cada número é par. E então para cada número par é adicionado a nova lista
+
+
+print (lista_par)
+    
+
+# Deixar listas em par
+
+
+lista_impar =[ x for x in b if x %3 == 0]
+
+print (lista_impar)
+
+# Deixar listas em impar'''
+
+
+'''matriz= [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+
+somas = [sum(linha) for linha in matriz] 
+
+# "sum(linha)" soma todos os elementos da linha interna. Para cada linha somada de dentro da matriz, a adiciona para a nova lista
+
+print (somas)'''
+
+
+
+print ("Ainda não terminei!!!!")
+    
